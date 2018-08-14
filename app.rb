88ec413b75f5e1ce62676c1f85c52707aa2d522a
AlinaHaha <2ndalina@gmail.com>
2018-08-14 19:15:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-
+get '/' do 
+  erb :index
 end
+
+get '/marnie' do
+  erb :marnie 
+end
+
+get '/alina' do
+  erb :alina
+end
+
+get '/rabiah' do
+  erb :rabiah
+end
+end 
